6c907f82263db77084ed96f8783b42ef7fb4f8b0
Jefferson Sia <jefferson.sia@razer.com>
2020-03-10 11:26:03 +0800

    Auth and login

--- cypress/integration/govtech_spec.js
+++ cypress/integration/govtech_spec.js
@@ -1,5 +1,25 @@
 describe('Business Grants', function () {
-    it('clicking "type" shows the right headings', function () {
-        cy.visit('')
+    it('basic auth', function () {
+        cy.fixture('authuser.json').as('authuser')
+        cy.get('@authuser').then(user => {
+            cy.visit('', {
+                auth: {
+                    username: user.username,
+                    password: user.password
+                }
+            })
+
+        })
+    })
+
+    it('can login', function () {
+        cy.get('#login-button > span').click()
+        cy.url().should('include', '/saml/sso_stub')
+        cy.get('[name="CPUID"]').clear().type('S1234567A')
+        cy.get('[name="CPUID_FullName"]').clear().type('Tan Ah Kow')
+        cy.get('[name="CPEntID"]').clear().type('BGPQEDEMO')
+
+        cy.get('[name="CPRole"]').select('Acceptor')
+        cy.get(':nth-child(8) > button').click()
     })
 })
